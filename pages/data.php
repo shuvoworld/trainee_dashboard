4a1f6fca5a30c07aa1f3877e8ac88d1b36741d63
shuvodu52 <shuvoworld@gmail.com>
2018-01-06 23:45:46 +0600

    initial commit

---
+++ pages/data.php
@@ -0,0 +1,39 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "training_db";
+$conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_query ($conn, "set character_set_client='utf8'"); 
+ mysqli_query ($conn, "set character_set_results='utf8'"); 
+
+ mysqli_query ($conn, "set collation_connection='utf8_general_ci'"); 
+	$sql = "SELECT
+    ins.institution_name as 'agency',
+	pr.project_name as 'project',
+	tr.trade_name AS 'trade',
+	s.student_name_bangla 'student_name_bangla',
+	s.father_name as 'father_name',
+	s.mother_name as 'mother_name',
+	s.mobile_number as 'mobile_number',
+	t.`name` AS 'training_center',
+	d.name_BN AS 'district'
+FROM
+	`training_student_info` AS s
+LEFT JOIN training_center AS t ON s.tcid = t.tcid
+LEFT JOIN district AS d ON t.district = d.id
+LEFT JOIN trade_info AS tr ON s.trade_name = tr.trade_id
+LEFT JOIN project_info AS pr ON s.project_id = pr.project_id
+LEFT JOIN institution as ins ON ins.ins_id  = pr.ins_id";
+$resultset = mysqli_query($conn, $sql) or die("database error:". mysqli_error($conn));
+$data = array();
+while( $rows = mysqli_fetch_assoc($resultset) ) {
+$data[] = $rows;
+}
+$results = array(
+"sEcho" => 1,
+"iTotalRecords" => count($data),
+"iTotalDisplayRecords" => count($data),
+"aaData"=>$data);
+echo json_encode($results);
+?>
---
+++ pages/index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>প্রশিক্ষণার্থী’র তথ্য</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="../vendor/datatables-plugins/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="../vendor/datatables-responsive/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="student.php">প্রশিক্ষন বাতায়ন থেকে সংগৃহীত প্রশিক্ষণার্থী’র তথ্য</a>
+            </div>
+            
+
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                    
+                        <li>
+                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="flot.html">Flot Charts</a>
+                                </li>
+                                <li>
+                                    <a href="morris.html">Morris.js Charts</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                        </li>
+                        <li>
+                            <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="panels-wells.html">Panels and Wells</a>
+                                </li>
+                                <li>
+                                    <a href="buttons.html">Buttons</a>
+                                </li>
+                                <li>
+                                    <a href="notifications.html">Notifications</a>
+                                </li>
+                                <li>
+                                    <a href="typography.html">Typography</a>
+                                </li>
+                                <li>
+                                    <a href="icons.html"> Icons</a>
+                                </li>
+                                <li>
+                                    <a href="grid.html">Grid</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="#">Second Level Item</a>
+                                </li>
+                                <li>
+                                    <a href="#">Second Level Item</a>
+                                </li>
+                                <li>
+                                    <a href="#">Third Level <span class="fa arrow"></span></a>
+                                    <ul class="nav nav-third-level">
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Third Level Item</a>
+                                        </li>
+                                    </ul>
+                                    <!-- /.nav-third-level -->
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+                            <ul class="nav nav-second-level">
+                                <li>
+                                    <a href="blank.html">Blank Page</a>
+                                </li>
+                                <li>
+                                    <a href="login.html">Login Page</a>
+                                </li>
+                            </ul>
+                            <!-- /.nav-second-level -->
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.sidebar-collapse -->
+            </div>
+            <!-- /.navbar-static-side -->
+        </nav>
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">প্রশিক্ষণার্থী’র তথ্য</h1>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            মন্ত্রণালয় এবং দপ্তর/সংস্থা থেকে অনলাইন এ সংগৃহীত তথ্য থেকে প্রাপ্ত প্রশিক্ষণার্থী’র তথ্য
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                <tr>
+                                <th>দপ্তর/সংস্থা</th>
+                                <th>প্রকল্প/ কর্মসুচি</th>
+                                <th>ট্রেড</th>
+                                <th>জেলা</th>
+                                <th>সেন্টার</th>
+                                <th>প্রশিক্ষণার্থী</th>
+                                <th>পিতা</th>
+                                <th>মাতা</th>
+                                <th>মোবাইল</th>
+                                </tr>
+                                </thead>
+                            </table>
+                            <!-- /.table-responsive -->
+                            <!-- <div class="well">
+                                <h4>DataTables Usage Information</h4>
+                                <p>DataTables is a very flexible, advanced tables plugin for jQuery. In SB Admin, we are using a specialized version of DataTables built for Bootstrap 3. We have also customized the table headings to use Font Awesome icons in place of images. For complete documentation on DataTables, visit their website at <a target="_blank" href="https://datatables.net/">https://datatables.net/</a>.</p>                        
+                            </div> -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            
+            
+            
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- DataTables JavaScript -->
+    <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+    <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+    <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="../dist/js/sb-admin-2.js"></script>
+
+    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
+    <script type="text/javascript">
+    $( document ).ready(function() {
+    var table = $('#dataTables-example').dataTable({
+                "bProcessing": true,
+                "sAjaxSource": "data.php",
+                "bPaginate":true,
+                "sPaginationType":"full_numbers",
+                "iDisplayLength": 10,
+                "aoColumns": [
+                    { mData: 'agency' } ,
+                    { mData: 'district' },
+                    { mData: 'project' },
+                    { mData: 'trade' },
+                    { mData: 'training_center' } ,
+                    { mData: 'student_name_bangla' } ,
+                    { mData: 'father_name' },
+                    { mData: 'mother_name' },
+                    { mData: 'mobile_number' }
+                            ]
+        });
+    });
+</script>
+
+</body>
+
+</html>
